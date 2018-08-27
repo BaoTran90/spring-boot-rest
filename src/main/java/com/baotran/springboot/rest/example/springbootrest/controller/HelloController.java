857acd0f381f4d69e688792e437985e4215437bb
Bao Tran <Bao.TranDuc@team.telstra.com>
2018-08-27 13:19:13 +0800

    feat: use Hystrix to handle timeout request

---
+++ src/main/java/com/baotran/springboot/rest/example/springbootrest/controller/HelloController.java
@@ -0,0 +1,22 @@
+package com.baotran.springboot.rest.example.springbootrest.controller;
+
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixProperty;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+    @GetMapping(value = "/hello")
+    @HystrixCommand(fallbackMethod = "fallback_hello", commandProperties = {
+            @HystrixProperty(name = "execution.isolation.thread.timeoutInMilliseconds", value = "1000")
+    })
+    public String hello() throws InterruptedException {
+        Thread.sleep(3000);
+        return "Welcome";
+    }
+
+    private String fallback_hello() {
+        return "Request fails. It takes long time to response";
+    }
+}
