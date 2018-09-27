ec7117bd0f3f8f0374a39c534c4039d04a6925ac
Bao Tran <ducbao90@gmail.com>
2018-09-27 09:52:41 +0700

    deploy: add deployment pipeline

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,9 +1,6 @@
 insert into student
 values(10001,'Ranga', 'E1234567');
 
-insert into student
-values(10002,'Ravi', 'A1234568');
-
 INSERT INTO USERS (ID, USERNAME,PASSWORD) VALUES (
    1, 'baotran','$2a$10$I33VQZOfpDsxw1zd2l4.l.Ofi5FDMgVG8x6D/QORGBqQJVv7hl7J6');
 
