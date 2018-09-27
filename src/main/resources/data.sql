31caeb87fc62d2c2d908c2d7c24774b90cd4b299
Bao Tran <ducbao90@gmail.com>
2018-09-27 10:04:05 +0700

    chore: update test data

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -5,7 +5,7 @@ insert into student
 values(10002,'Ravi', 'A1234568');
 
 insert into student
-values(10003,'', 'A1234568Ba9');
+values(10003,'bao', 'A1234568Ba9');
 
 INSERT INTO USERS (ID, USERNAME,PASSWORD) VALUES (
    1, 'baotran','$2a$10$I33VQZOfpDsxw1zd2l4.l.Ofi5FDMgVG8x6D/QORGBqQJVv7hl7J6');
